e25365aa31c4c12a5df2f47174ffacd695dcf806
Harald Wellmann <harald.wellmann@gmx.de>
2013-05-25 11:32:59 +0200

    failing test for rollback problem

---
+++ orient-spring-tx/src/test/java/org/ops4j/orient/spring/tx/object/TransactionalAutoRollbackTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2013 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ops4j.orient.spring.tx.object;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.ops4j.orient.spring.tx.OrientObjectDatabaseFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.BeforeTransaction;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * This test verifies transaction propagation and rollback behaviour. The test class is transactional,
+ * i.e. each test method is wrapped in a transaction which is started before the test method 
+ * and rolled back by the test container after the test method.
+ * <p>
+ * The methods of this test class need to be executed in the given order
+ * 
+ * @author Harald Wellmann
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ObjectSpringTestConfig.class)
+@Transactional
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TransactionalAutoRollbackTest {
+
+    @Autowired
+    private TransactionalObjectService service;
+
+    @Autowired
+    private OrientObjectDatabaseFactory dbf;
+
+    @BeforeTransaction
+    public void setUp() {
+        service.registerEntityClasses();
+    }
+
+    @Test
+    public void test01DbShouldBeEmpty() {
+        assertThat(service.count(), is(0L));
+    }
+
+    @Test
+    public void test02SaveOneObject() {
+        assertThat(service.count(), is(0L));
+        service.commitAutomatically();
+        assertThat(service.count(), is(1L));
+    }
+
+    @Test
+    public void test03DbShouldBeEmpty() {
+        assertThat(service.count(), is(0L));
+    }
+}
