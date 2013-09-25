9a582fb9042e4efe4f6ca4f5a8169da8ea16ac4b
Harald Wellmann <harald.wellmann@gmx.de>
2013-09-25 23:52:00 +0200

    [ORIENT-9] OrientGraphFactory for new Blueprints API

---
+++ orient-spring-tx/src/main/java/org/ops4j/orient/spring/tx/OrientGraphFactory.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2013 Harald Wellmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ops4j.orient.spring.tx;
+
+import com.orientechnologies.orient.core.db.ODatabasePoolBase;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentPool;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+
+/**
+ * Factory for OrientDB graphs with Blueprints API.
+ * 
+ * @author Harald Wellmann
+ * 
+ */
+public class OrientGraphFactory extends AbstractOrientDatabaseFactory {
+
+    private ODatabaseDocumentTx db;
+    private ODatabasePoolBase<ODatabaseDocumentTx> pool;
+
+    @Override
+    protected void createPool() {
+        pool = new ODatabaseDocumentPool(getUrl(), getUsername(), getPassword());
+        pool.setup(getMinPoolSize(), getMaxPoolSize());        
+    }
+
+
+    @Override
+    public ODatabaseDocumentTx openDatabase() {
+        db = pool.acquire();
+        return db;
+    }
+    
+    public ODatabaseDocumentTx db() {
+        return (ODatabaseDocumentTx) super.db();
+    }
+    
+    public OrientGraph graph() {
+        return new OrientGraph((ODatabaseDocumentTx) super.db());
+    }
+    
+
+    protected ODatabaseDocumentTx newDatabase() {
+        return new ODatabaseDocumentTx(getUrl());
+    }
+    
+}
